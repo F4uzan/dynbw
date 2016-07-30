e956e8eb24d6b299772e6a9b25ecf579bd1ac203
F4uzan <derpish123@gmail.com>
2016-07-30 14:24:10 +0700

    install: allow deletion of preexisting dynbw.sh
    Let's assume there is a dynbw.sh in install directory, with all possibility, the only reason it would be there is either because of user has installed it once or user copied it there manually.
    
    So, we're safe to delete it since it would be replaced by whatever version exists currently.

--- install.sh
+++ install.sh
@@ -5,7 +5,16 @@
 read -p "Folder to install DynBW to: " install_to
 if [ -e $install_to/dynbw.sh ]; then
 	echo "dynbw.sh already exists in installation directory!"
-	echo "Please delete or move the file to continue installation"
+	read -p "Do you want to delete the conflicting file [y/N]? " delete
+	if [ $delete == "y" ]; then
+		rm $install_to/dynbw.sh
+		echo "File deleted!"
+		echo "Continuing installation.."
+		ln -s $PWD/dynbw.sh $install_to/dynbw.sh
+		echo "Installation complete! Launch build.sh in $install_to to use DynBW"
+	else
+		echo "Please delete or move the file to continue installation"
+	fi
 	exit
 else
 	ln -s $PWD/dynbw.sh $install_to/dynbw.sh
