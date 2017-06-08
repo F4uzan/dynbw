06347c4eec0d1e5e6580faa45ae6098a688ab097
F4uzan <f4uzan@this.is.f4>
2017-06-08 14:42:28 +0700

    dynbw_2: Go back to using switch-case
    We quickly switched to if-statement in c48acc8 but we can make the guard
    work with our original switch-case implementation as well.

--- dynbw_2.sh
+++ dynbw_2.sh
@@ -106,13 +106,14 @@ config() {
 			echo
 			printf "How many CPU cores should the build system use? "
 			read -r p_cores
-			echo "$p_cores"
-			if expr "$p_cores" : '[0-9][0-9]*$'>/dev/null; then
-				printf "cores=%s\n" "$p_cores" >> "$conf_file"
-			else
+			case "$p_cores" in
+			''|*[!0-9]*) 
 				echo "/!\ Invalid input detected, using automated cores detection instead"
 				printf "cores=auto\n" >> "$conf_file"
-			fi
+				;;
+			*)
+				printf "cores=%s\n" "$p_cores" >> "$conf_file"
+				;;
 		esac
 		fi
 		echo
