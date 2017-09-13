F4uzan/dynbw

commit 26a7e0389dde3953756ffc6318cb911d2e4ae5ba

dynbw: Fix sync on ZSH
ZSH was showing weird behaviour with ‘cat sync.txt’ and it was unable to parse sync.txt properly that way. Using ‘<sync.txt’ didn’t help either, so it’s about time to rework the for loop and change it into something dirtier but works better across multiple shells.

*TODO: I doubt this will actually work in the regular SH/Shell, I’ll have to test its portability later.

dynbw.sh
@@ -428,7 +428,6 @@ dynbw() {
 			echo "/!\ Cannot find sync.txt in the current directory"
 			return
 		fi
-		input_fetch="$(cat sync.txt)"
 		case "$c_arg" in
 		--help|-h)
 			echo "usage: dynbw sync [<argument>]"
@@ -446,7 +445,7 @@ dynbw() {
 			;;
 		esac
 		IFS="$(printf '%b_' '\n')"; IFS="${IFS%_}"
-		for line in $input_fetch; do
+		sed '/^[ \t]*$/d' sync.txt | while read -r line; do
 			first_char="$(echo "$line" | head -c1)"
 			if [ "$first_char" = "/" ]; then
 				cmd="$(echo "$line" | cut -d":" -f1)"

commit 4e63fb9d9c79ce116aa0b941607f2f21049fd6cc

dynbw: We don’t use $cmd anymore
Sync broke in 26a7e0389dde3953756ffc6318cb911d2e4ae5ba because we changed $cmd to $com. Fix the $cmd leftovers not covered in 26a7e0389dde3953756ffc6318cb911d2e4ae5ba.

dynbw.sh
@@ -450,7 +450,7 @@ dynbw() {
 			if [ "$first_char" = "/" ]; then
 				com="$(echo "$line" | cut -d":" -f1)"
 				arg="$(echo "$line" | cut -d":" -f2)"
-				case $cmd in
+				case $com in
 				/save_to|"/ save_to")
 					dir="$(echo "$arg" | xargs)"
 					if [ ! -d "$dir" ]; then